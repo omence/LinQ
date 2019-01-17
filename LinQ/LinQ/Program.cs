0900e648bb9c8fe2563b48fbcfffa0e0610e0745
Jason Few <omence11@gmail.com>
2019-01-17 13:21:12 -0800

    Got some done

--- LinQ/LinQ/Program.cs
+++ LinQ/LinQ/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using LinQ.Classes;
+using Newtonsoft.Json;
 
 namespace LinQ
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            //C: \Users\jason\Source\Repos\LinQ\LinQ.JSON
+
+            string path = "../../../../../LinQ.JSON";
             Console.WriteLine("Hello World!");
+            FeaturesJS(path);
+        }
+
+        public static void FeaturesJS(string path)
+        {
+            string data = "";
+            using (StreamReader sr = File.OpenText(path))
+            {
+                data = sr.ReadToEnd();
+            }
+            Feature feature = JsonConvert.DeserializeObject<Feature>(data);
+
+
+
+            //IEnumerable<string> neighborhoods = from n in feature.properties
+            //                                    where n.Contains(neighborhood)
+            //                                    select n;
+
         }
     }
 }
